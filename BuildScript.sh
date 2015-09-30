840b86e80efba61a4441c99cc1fb3c0217ca9ad0
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2015-09-30 14:52:40 +0100

    build with cure project path

--- BuildScript.sh
+++ BuildScript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Building Unity3d Game..."
+echo "Building Unity3d Project..."
+DIR=`pwd`
 UNITY3D_LOC="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
-eval $UNITY3D_LOC -quit -batchmode -executeMethod ToBuild.BuildLinux
+
+eval $UNITY3D_LOC -quit -batchmode -projectPath $DIR -executeMethod ToBuild.BuildLinux
+
 echo "Build Complete!"
