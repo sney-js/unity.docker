249baa034b9a533919131798a082a93c49730751
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2015-09-30 17:47:19 +0100

    Git Reset before building to get latest update.

--- BuildScript.sh
+++ BuildScript.sh
@@ -4,6 +4,9 @@ echo "Building Unity3d Project..."
 DIR=`pwd`
 UNITYLOC="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 
+GITRESET="eval git reset --hard origin/master"
+eval GITRESET
+
 BUILD="$UNITYLOC -quit -batchmode -projectPath $DIR -logFile $DIR"/Build.log"  -executeMethod ToBuild.BuildLinux"
 if eval $BUILD
 	then echo "Build Complete!"
