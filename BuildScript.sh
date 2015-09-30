fad812e97bd3a7007a91644a53f19407fadb311a
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2015-09-30 15:13:55 +0100

    username and password params

--- BuildScript.sh
+++ BuildScript.sh
@@ -3,7 +3,9 @@
 echo "Building Unity3d Project..."
 DIR=`pwd`
 UNITY3D_LOC="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
+USER="ssnehil@hotmail.com"
+PASS="Unity6558"
 
-eval $UNITY3D_LOC -quit -batchmode -projectPath $DIR -executeMethod ToBuild.BuildLinux
+eval $UNITY3D_LOC -quit -batchmode -projectPath $DIR -username $USER -password $PASS -executeMethod ToBuild.BuildMac
 
 echo "Build Complete!"
