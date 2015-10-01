de212409d0e81ba4d855353e2d12f00e6420fa92
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2015-10-01 16:00:51 +0100

    GO ready script. params work and helpful output messages.

--- BuildScript.sh
+++ BuildScript.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
-echo "... Checking git"
 
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse @{u})
-BASE=$(git merge-base @ @{u})
+while [[ $# > 1 ]]
+do
+	key="$1"
 
-if [ $LOCAL = $REMOTE ]; then
-    echo "... GIT Up-to-date. New Build not required."
-elif [ $LOCAL = $BASE ]; then
-	
-	echo "... New commit detected! Pulling..."
-	GITFETCH="git fetch"
-	GITRESET="eval git reset --hard origin/master"
-	eval $GITFETCH
-	eval $GITRESET  
-	
-	DIR=`pwd`
+	case $key in
+	    -platform)
+		    BUILD_PLATFORM="$2"
+		    shift # past argument
+	    ;;
+	    -unityPath)
+		    UNITYLOC="$2"
+		    shift # past argument
+	    ;;
+	    *)	
+	            # unknown option
+	    ;;
+	esac
+	shift # past argument or value
+done
+
+if [ "$UNITYLOC" == "" ]; then 
 	UNITYLOC="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
+fi
 
 
-	echo "... Re-Building Unity3d Project"
-	BUILD="$UNITYLOC -quit -batchmode -projectPath $DIR -logFile $DIR'/Build.log'  -executeMethod ToBuild.BuildLinux"
-	if eval $BUILD
-		then echo "... Build Complete!"
-	else echo "... An Error Occurred."
-	fi
+if [ "$BUILD_PLATFORM" == "win" ]; then   
+	BUILD_METHOD="BuildWindows"
+elif [ "$BUILD_PLATFORM" == "mac" ]; then   
+	BUILD_METHOD="BuildMac"
+elif [ "$BUILD_PLATFORM" == "linux" ]; then   
+	BUILD_METHOD="BuildLinux" 
+else
+	BUILD_METHOD="BuildLinux"
+fi
 
+echo "Build for : "$BUILD_PLATFORM
+echo "Unity Location : "$UNITYLOC
+
+echo "... Building Unity3d Project"
+
+DIR=`pwd`
+BUILD="$UNITYLOC -quit -batchmode -projectPath $DIR -logFile $DIR'/Build.log' -executeMethod ToBuild.$BUILD_METHOD"
+	
+if eval $BUILD
+	then echo "... Build Complete!"
+else echo "... An Error Occurred."
 fi
