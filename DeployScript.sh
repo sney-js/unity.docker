8bf7ac663b5db58cfacc0e3a5a3c9a558ff3cc28
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2015-10-05 11:13:54 +0100

    cleaning and extracting...

--- DeployScript.sh
+++ DeployScript.sh
@@ -11,7 +11,7 @@ vmhost="10.2.21.63"
 sshKey="stardock_keys"
 configDir="binaries" #/Snehil.Docker.game_SpaceDocker_Game_BuildDocker_md5.checksum ?
 binarydir="binaries/*"
-echo "hee"
+
 while [[ $# > 1 ]]
 do
     scriptKey="$1"
@@ -34,80 +34,18 @@ do
     esac
     shift
 done
+
 binarydir="$binarydir""*"
-echo "HELLOW"
-echo $binarydir
+echo "Binary Dir: $binarydir"
+
+echo "[$me] Compressing $configDir"
 COMPRESS="tar cvpf $ARCHIVENAME $binarydir"
 if $COMPRESS; then
-    echo "... compressed binary. Connecting to server..."
-    echo "[$me] Cleaning staging dir..."
-    # ssh -o StrictHostKeyChecking=no -i $sshKey stardock@$vmhost "cd $configDir; rm -rf staging/*"
+    echo "[$me] Cleaning $vmhost dir..."
+    ssh -o StrictHostKeyChecking=no -i $sshKey stardock@$vmhost "cd $configDir; rm -rf *"
     echo "[$me] Deploying $ARCHIVENAME to stardock@$vmhost:$configDir ..."
     scp -oStrictHostKeyChecking=no -i $sshKey $ARCHIVENAME stardock@$vmhost:$configDir
-    #echo "[$me] Verifying configuration..."
-    #ssh -o StrictHostKeyChecking=no -i $sshKey stardock@$vmhost "cd $configDir/staging; tar -xvf $ARCHIVENAME"
+    echo "[$me] Extracting..."
+    ssh -oStrictHostKeyChecking=no -i $sshKey stardock@$vmhost "cd $configDir; tar -xvf $ARCHIVENAME"
     echo "[$me] Completed"
 fi
-
-
-
-
-
-
-
-# Defaults
-#Please don't modify them directly. Use script keys instead. 
-#Example: DeployScript.sh --ssh-key nagios_key --file nagios-config.tar --target-host ldnxvmon02.emea.akqa.local --config-dir /etc/nagios3
-
-
-
-
-
-# me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
-
-# while [ $# > 1 ]
-# do
-# scriptKey="$1"
-# shift
-
-# case $scriptKey in
-#     -k|--ssh-key)
-#     sshKey="$1"
-#     shift
-#     ;;
-#     -f|--file)
-#     archiveName="$1"
-#     shift
-#     ;;
-#     -t|--target-host)
-#     vmhost="$1"
-#     shift
-#     ;;
-#     -c|--config-dir)
-#     configDir="$1"
-#     shift
-#     ;;
-# 	-h|--help)
-#     echo "[-k|--ssh-key] ssh_key - specify ssh key to use"
-# 	echo "[-f|--file] file.tar - specify file to deploy"
-# 	echo "[-t|--target-host] hostname - specify target nagios host deploy"
-# 	echo "[-c|--config-dir] file.tar - specify nagios3 config dir. By default will be used /etc/nagios3 dir"
-#     exit 0
-#     ;;
-#     *)
-# 	break
-#     ;;
-# esac
-# done
-
-# # Hack to fix WARNING: UNPROTECTED PRIVATE KEY FILE! error
-# chmod 600 $sshKey
-
-
-# echo "[$me] Cleaning staging dir..."
-# ssh -oStrictHostKeyChecking=no -i $sshKey nagios@$nagiosHost "cd $configDir; rm -rf staging/*"
-# echo "[$me] Deploying $archiveName to nagios@$nagiosHost:$configDir ..."
-# scp -oStrictHostKeyChecking=no -i $sshKey $archiveName nagios@$nagiosHost:$configDir/staging
-# echo "[$me] Verifying configuration..."
-# ssh -oStrictHostKeyChecking=no -i $sshKey nagios@$nagiosHost "cd $configDir/staging; tar -xvf $archiveName; rm conf.d/pnp4nagios.cfg; ln -s /etc/pnp4nagios/nagios.cfg conf.d/pnp4nagios.cfg; cd go; sh go_verify_config.sh"
-# echo "[$me] Compleated"
