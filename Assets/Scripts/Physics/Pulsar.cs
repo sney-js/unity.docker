5a9a83b7c29c26acc5b8f7ddb84baf09a71ff13a
Snehil Bhushan <snehil.bhushan@akqa.com>
2016-01-07 23:35:50 +0000

    pulsars. lev 3 overamped

--- Assets/Scripts/Physics/Pulsar.cs
+++ Assets/Scripts/Physics/Pulsar.cs
@@ -4,18 +4,24 @@ using System.Collections;
 public class Pulsar : MonoBehaviour
 {
 
-	float rotationsPerMinute = 50f;
-	float sleep = 0f;
+	public float speed = 0f;
+	public float XValue = 100f;
+	public float YValue = 50f;
+	public float ZValue = 0f;
 	// Update is called once per frame
-	void Update ()
+
+	void LateUpdate ()
 	{
-		if (Time.time > sleep + 2f) {
-			float t = Time.smoothDeltaTime;
-			t = Mathf.Sin (t * Mathf.PI * 0.5f);
-			transform.Rotate (new Vector3 (0f, 6.0f * rotationsPerMinute * t, 0f));
-		} 
-		else if (Mathf.Approximately (transform.eulerAngles.y, 180f)) {
-			sleep = Time.time;
-		}
+		float t = Time.smoothDeltaTime;
+		t = Mathf.Sin (t * Mathf.PI * 0.5f);
+		Vector3 rot = Vector3.zero;
+		rot.y = Mathf.Sin(2*YValue);
+		rot.x = Mathf.Cos(XValue);
+		rot.z = Mathf.Cos(2*ZValue);
+		rot *= speed;
+		transform.Rotate (rot);
 	}
+
+
+
 }
