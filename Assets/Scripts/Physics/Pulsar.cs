80ec2edb0e9d0ab9b272e00ebacb2dca2a1a4b44
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2016-02-03 17:31:31 +0000

    level 3 fix.

--- Assets/Scripts/Physics/Pulsar.cs
+++ Assets/Scripts/Physics/Pulsar.cs
@@ -5,21 +5,21 @@ public class Pulsar : MonoBehaviour
 {
 
 	public float speed = 0f;
-	public float XValue = 100f;
-	public float YValue = 50f;
-	public float ZValue = 0f;
+	public bool XValue = false;
+	public bool YValue = false;
+	public bool ZValue = true;
 	// Update is called once per frame
 
 	void LateUpdate ()
 	{
-		float t = Time.smoothDeltaTime;
-		t = Mathf.Sin (t * Mathf.PI * 0.5f);
-		Vector3 rot = Vector3.zero;
-		rot.y = Mathf.Sin(2*YValue);
-		rot.x = Mathf.Cos(XValue);
-		rot.z = Mathf.Cos(2*ZValue);
-		rot *= speed;
-		transform.Rotate (rot);
+		if (Time.timeScale > 0) {
+			Vector3 rot = Vector3.zero;
+			rot.x += XValue ? 1 : 0;
+			rot.y += YValue ? 1 : 0;
+			rot.z += ZValue ? 1 : 0;
+			rot *= speed;
+			transform.Rotate (rot);
+		}
 	}
 
 
